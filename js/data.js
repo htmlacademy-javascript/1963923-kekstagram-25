bb51d18beebe86455ed2c989fbff3b249d98176a
Sergeyev Yevgeniy <sergeyeven@gmail.com>
2022-03-07 17:36:17 +0300

    Перенос переменных в модуль генерации карточек

--- js/data.js
+++ js/data.js
@@ -1,20 +1,45 @@
 import {
-  DESCRIPTIONS,
-  MESSAGES,
-  NAMES,
-  MIN_AVATAR,
-  MAX_AVATAR,
-  MIN_COMMENTS_COUNT,
-  MAX_COMMENTS_COUNT,
-  MIN_LIKES_COUNT,
-  MAX_LIKES_COUNT
-} from './const.js';
-
-import {
   getRandomInt,
   getRandomArrayElement,
   getRandomArray
 } from './util.js';
+
+const DESCRIPTIONS = [
+  'Поездка по Санкт-Питербургу',
+  'Золотая осень',
+  'Моя любимая собака',
+  'Рыбалка это моё хобби',
+  'Поездка на Волгу',
+  'Поездка на море',
+];
+
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const NAMES = [
+  'Владимир',
+  'Елена',
+  'Ксения',
+  'Владислав',
+  'Егор',
+  'Сергей',
+  'Наталья',
+  'Евгений',
+  'Дарья'
+];
+
+const MIN_AVATAR = 1;
+const MAX_AVATAR = 6;
+const MIN_COMMENTS_COUNT = 0;
+const MAX_COMMENTS_COUNT = 5;
+const MIN_LIKES_COUNT = 15;
+const MAX_LIKES_COUNT = 200;
 
 const getComment = (id) => {
   const comment = {
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-import {PHOTOS_COUNT} from './const.js';
 import {getPhotos} from './data.js';
 import {getLengthOfString} from './util.js';
+
+const PHOTOS_COUNT = 25;
 
 getPhotos(PHOTOS_COUNT);
 getLengthOfString('qwerty', 10);
